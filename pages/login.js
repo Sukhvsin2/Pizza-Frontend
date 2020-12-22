5296c1d96e09f242d11771933825684302630280
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-12-23 01:06:52 +0530

    Login In-progress

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from '../styles/Login.module.css'
 
 function login() {
     return (
-        <div>
+        <div className={styles.container}>
             Login
         </div>
     )
